882bcadc5055e3b23d9cbb17ae2063d711933c97
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2018-08-21 08:18:44 +0200

    Update get_current_version.sh

--- bash/get_current_version.sh
+++ bash/get_current_version.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
   
+# user or org in vagrant cloud
 echo The box of which user do you want?
-
 read var1
 
+# box in user or org in vagrant cloud
 echo The url of which box of this user do you want?
-
 read var2
 
-curl -sL \
-  https://app.vagrantup.com/api/v1/box/$var1/$var2 | jq '.current_version.providers[0] .download_url'
+# grab user/box json
+# filter current version
+# grab first privider
+# grab downloar_url
 
-echo $?
+curl -sL \
+  https://app.vagrantup.com/api/v1/box/${var1}/${var2} | jq '.current_version.providers[0] .download_url'
+
+echo ${?}
